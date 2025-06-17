088f8a4db16782a5e3c01b7c67f8b2f9006a4ea1
Pablo Leyton <pleytonsoftware@gmail.com>
2025-06-17 16:06:52 +0200

    feat: add vite-tsconfig-paths for improved path resolution and update configuration files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import './App.css'
+import { cn } from '@cn'
 
-import { cn } from './utils/cn'
+import './App.css'
 
 function App() {
 	return <h1 className={cn('w-full text-center text-3xl font-bold underline')}>Hello world!</h1>
