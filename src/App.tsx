003b033780ec709bd0841301ec1987ae2242672d
Pablo Leyton <pleytonsoftware@gmail.com>
2025-06-17 16:02:37 +0200

    feat: implement utility function for class name merging and update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import './App.css'
 
+import { cn } from './utils/cn'
+
 function App() {
-	return <h1 className='w-full text-center text-3xl font-bold underline'>Hello world!</h1>
+	return <h1 className={cn('w-full text-center text-3xl font-bold underline')}>Hello world!</h1>
 }
 
 export default App
