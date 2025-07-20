ffc3159b4ac9f8bc2a5fc47cae34996330bb6566
Pablo Leyton <pleytonsoftware@gmail.com>
2025-07-20 13:06:40 +0200

    feat: Add country flags and enhance language selector component

---
+++ src/assets/flags/index.ts
@@ -0,0 +1,94 @@
+import type { AVAILABLE_LANGUAGE_CODES, DISABLED_LANGUAGE_CODES, LANGUAGE_CODES } from '@const/languages'
+import type { ImageProps } from 'next/image'
+import type { ComponentType, SVGProps } from 'react'
+
+import arab from './arab.svg'
+import bg from './bg.svg'
+import cn from './cn.svg'
+import cz from './cz.svg'
+import de from './de.svg'
+import dk from './dk.svg'
+import ee from './ee.svg'
+import esCt from './es-ct.svg'
+import esGa from './es-ga.svg'
+import esPv from './es-pv.svg'
+import es from './es.svg'
+import fi from './fi.svg'
+import fr from './fr.svg'
+import gb from './gb.svg'
+import gr from './gr.svg'
+import hr from './hr.svg'
+import hu from './hu.svg'
+import id from './id.svg'
+import il from './il.svg'
+import inFlag from './in.svg'
+import it from './it.svg'
+import jp from './jp.svg'
+import kr from './kr.svg'
+import lt from './lt.svg'
+import lv from './lv.svg'
+import my from './my.svg'
+import nl from './nl.svg'
+import no from './no.svg'
+import pl from './pl.svg'
+import pt from './pt.svg'
+import ro from './ro.svg'
+import rs from './rs.svg'
+import ru from './ru.svg'
+import se from './se.svg'
+import si from './si.svg'
+import sk from './sk.svg'
+import th from './th.svg'
+import tr from './tr.svg'
+import ua from './ua.svg'
+import vn from './vn.svg'
+
+export const AVAILABLE_LANGUAGE_FLAGS_DICTIONARY: Record<AVAILABLE_LANGUAGE_CODES, ImageProps> = {
+	en: gb,
+	de: de,
+	fr: fr,
+	es: es,
+	it: it,
+	pt: pt,
+	ca: esCt,
+	eu: esPv,
+	gl: esGa,
+	ar: arab, // Using 'arab' for Arabic flag
+}
+export const DISABLED_LANGUAGE_FLAGS_DICTIONARY: Record<DISABLED_LANGUAGE_CODES, ImageProps> = {
+	nl: nl,
+	pl: pl,
+	ru: ru,
+	zh: cn, // Using 'cn' for Chinese flag
+	ja: jp, // Using 'jp' for Japanese flag
+	ko: kr, // Using 'kr' for Korean flag
+	hi: inFlag, // Using 'inFlag' for Indian flag
+	tr: tr,
+	vi: vn, // Using 'vn' for Vietnamese flag
+	id: id,
+	th: th,
+	ms: my, // Using 'my' for Malay flag
+	cs: cz,
+	hu: hu,
+	ro: ro,
+	sk: sk,
+	bg: bg,
+	hr: hr,
+	sr: rs, // Using 'rs' for Serbian flag
+	uk: ua, // Using 'ua' for Ukrainian flag
+	el: gr, // Using 'gr' for Greek flag
+	fi: fi,
+	no: no,
+	da: dk, // Using 'dk' for Danish flag
+	sv: se, // Using 'se' for Swedish flag
+	lt: lt,
+	sl: si, // Using 'si' for Slovenian flag
+	et: ee, // Using 'ee' for Estonian flag
+	lv: lv, // Using 'lv' for Latvian flag
+	he: il,
+}
+
+export const ALL_LANGUAGE_FLAGS_DICTIONARY: Record<LANGUAGE_CODES, ImageProps> = {
+	...AVAILABLE_LANGUAGE_FLAGS_DICTIONARY,
+	...DISABLED_LANGUAGE_FLAGS_DICTIONARY,
+}
--- src/components/ui/molecules/language-selector/language-selector.tsx
+++ src/components/ui/molecules/language-selector/language-selector.tsx
@@ -2,6 +2,7 @@
 
 import { redirect, usePathname } from '@navigation'
 
+import Image from 'next/image'
 import { useSearchParams } from 'next/navigation'
 import { useMemo } from 'react'
 
@@ -11,6 +12,8 @@ import { Dropdown } from '@atoms/dropdown'
 import { AVAILABLE_LANGUAGES, LOCALE_DICTIONARY } from '@const/languages'
 import { useLanguage } from '@hooks/use-language'
 
+import { AVAILABLE_LANGUAGE_FLAGS_DICTIONARY } from '@/assets/flags'
+
 export const LanguageSelector = () => {
 	const language = useLanguage()
 	const path = usePathname()
@@ -18,23 +21,33 @@ export const LanguageSelector = () => {
 
 	const languagesNode = useMemo(
 		() =>
-			AVAILABLE_LANGUAGES.map((lang) => (
-				<Dropdown.Item
-					key={lang}
-					onClick={() =>
-						redirect({
-							href: {
-								pathname: path,
-								query: Object.fromEntries(params.entries()),
-							},
-							locale: lang,
-						})
-					}
-					selected={language === lang}
-				>
-					<span>{LOCALE_DICTIONARY[lang]}</span>
-				</Dropdown.Item>
-			)),
+			AVAILABLE_LANGUAGES.map((lang) => {
+				const flagProps = AVAILABLE_LANGUAGE_FLAGS_DICTIONARY[lang]
+				const langName = LOCALE_DICTIONARY[lang]
+				const { src, width, height } = flagProps || {}
+
+				return (
+					<Dropdown.Item
+						key={lang}
+						onClick={() =>
+							redirect({
+								href: {
+									pathname: path,
+									query: Object.fromEntries(params.entries()),
+								},
+								locale: lang,
+							})
+						}
+						selected={language === lang}
+						className='max-w-1/2'
+					>
+						<div>
+							{flagProps && <Image {...{ src, width, height }} className='size-3' alt={langName} />}
+							<span className='truncate'>{langName}</span>
+						</div>
+					</Dropdown.Item>
+				)
+			}),
 		[AVAILABLE_LANGUAGES],
 	)
 
@@ -43,7 +56,7 @@ export const LanguageSelector = () => {
 			vertical='top'
 			horizontal='end'
 			buttonClassName='shadow-lg'
-			menuClassName='max-h-60 overflow-y-auto'
+			menuClassName='w-64 max-h-60 overflow-y-auto'
 			buttonContent={
 				<Dropdown.Item className='flex items-center gap-2 text-sm font-medium text-base-content'>
 					<LanguagesIcon className='text-primary w-4 h-4' />
